e0f3c0d672114d1781a3b7ae02aad29a34b5c1aa
Tri <triphan921@gmail.com>
2021-02-22 11:56:07 -0800

    commit tree a5a5ab02866be21a46693ce8397de4661da5c847

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  if (ride > 2500) {
+    return 'No can do.'
+  }
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(x) {
+  if (x === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(x){
+  if (x === 'generous') {
+    return 'Thank you so much.'
+  } else if (x === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
